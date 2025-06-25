3fb68ddeb74316d9fd6f8c0ca3a3881f99091a2b
rabindarlami <rabindarlami15905@gmail.com>
2025-06-25 15:58:32 +0545

    Restore public folder from errorHandling commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,22 +3,14 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
-import { z } from 'zod';
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
-
-// import { id } from 'zod/v4/locales';
+import { z } from 'zod'
+import { id } from 'zod/v4/locales';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string({
-        invalid_type_error: 'Customer ID is required',
-    }),
-    amount: z.coerce
-    .number().gt(0, {message: 'Amount must be greater than 0'}),
-    status: z.enum(['pending', 'paid'],{
-        invalid_type_error: 'Please select an invoice status',
-    }),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
     date: z.string(),
 })
 
@@ -28,31 +20,13 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export type State = {
-  errors?: {
-    customerId?: string[];
-    amount?: string[];
-    status?: string[];
-  };
-  message?: string | null;
-};
-
 //creating invoices
-export async function createInvoice(prevState: State, formData: FormData) {
-    const  validatedFields = CreateInvoice.safeParse({
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     })
-
-     if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
-    };
-  }
-
-  const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -96,23 +70,4 @@ export async function deleteInvoice(id: string) {
      throw new Error('Failed to Delete Invoice');
     await sql`DELETE FROM invoices WHERE id = ${id}`
     revalidatePath('/dashboard/invoices')
-}
-
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
 }
